56e6bd89dd8884941c9e0ce56fb8cc1d7756a36c
Mahendra Yadav <mahendra.k12@gmail.com>
2019-03-27 00:18:20 +0530

    todo app v1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <todo-list v-bind:todos="todos"></todo-list>
+			<create-todo v-on:add-todo="addTodo"></create-todo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import sweetalert from 'sweetalert';
+import TodoList from './components/TodoList.vue'
+import CreateTodo from './components/CreateTodo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    TodoList,
+		CreateTodo
+  },
+	data() {
+		return {
+			todos: [{
+				id: 1,
+				title: "Todo A",
+				project: "Project A",
+				done: false
+			}, {
+				id: 2,
+				title: "Todo B",
+				project: "Project B",
+				done: false
+			}, {
+				id: 3,
+				title: "Todo C",
+				project: "Project C",
+				done: false
+			}, {
+				id: 4,
+				title: "Todo D",
+				project: "Project D",
+				done: false
+			}]
+		}
+	},
+	methods: {
+		addTodo(newTodo) {
+			this.todos.push(newTodo);
+			sweetalert('Success!', 'Task created!', 'success');
+		},
+	},
 }
 </script>
 
---
+++ src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+		<p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+		<todo v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo.id" v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo"></todo>
+	</div>
+</template>
+
+<script type="text/javascript">
+
+import sweetalert from 'sweetalert';
+import Todo from "./Todo.vue";
+
+export default {
+	props: ['todos'],
+	components: {
+		Todo,
+	},
+	methods: {
+		deleteTodo(todo){
+			const todoIndex = this.todos.indexOf(todo);
+			this.todos.splice(todoIndex, 1);
+			sweetalert('Deleted!', 'Your Task has been deleted.', 'success');
+		},
+		completeTodo(todo){
+			const todoIndex = this.todos.indexOf(todo);
+			this.todos[todoIndex].done = true;
+			sweetalert('Success!', 'Task completed!', 'success');
+		},
+	},
+};
+</script>
+
+<style></style>
+
+
